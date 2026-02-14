cbfbf4163633f00823d2711c2e4051378f618f1d
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-14 12:13:03 +0530

    Add a beautiful button

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -12,11 +12,14 @@ const Hero = ()=>{
                 <h1 className={`${outfit.className} max-w-2xl text-7xl font-medium tracking-tighter text-center bg-clip-text bg-linear-to-b from-neutral-50 to-neutral-500 text-transparent`}>
                     Unleash the Power of intutive finance
                 </h1>
-                <p className="text-sm text-center max-w-3xl mt-12 text-neutral-200">
+                <p className="text-sm max-w-2xl mx-auto text-center mt-12 text-neutral-300">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quo maiores deleniti accusamus est, placeat ut dolorem, optio magni accusantium eius? Recusandae ullam praesentium accusamus necessitatibus vitae, veritatis delectus minus.
                 </p>
+                <div>
+                    <button className={`${outfit.className} bg-violet-600 px-4 py-2 rounded-2xl cursor-pointer hover:bg-violet-500 hover:scale-105 transition-all duration-100 mt-12`}>Join the Waitlist</button>
+                </div>
 
-            </div>
+            </div>  
         </section>
     )
 }
