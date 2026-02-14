d46550c65f7fbd3626d54a3d5c58d58c76b8d5a0
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-14 11:50:01 +0530

    Add Main hero Text

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import Hero from "./sections/Hero";
 export default function Home() {
-  return <div className="container">
-    <Hero />
-  </div>;
+  return (
+    <>
+      <section className="">
+        <Hero />
+      </section>
+    </>
+  );
 }
--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -8,8 +8,13 @@ const outfit = Outfit({
 const Hero = ()=>{
     return(
         <section className="container">
-            <div className="h-screen flex items-center justify-center bg-violet-400">
-                <h1 className={`${outfit.className} text-5xl font-medium tracking-tighter text-center`}>Welcome to My Website</h1>
+            <div className="text-white h-screen flex flex-col items-center my-[-100] justify-center">
+                <h1 className={`${outfit.className} max-w-2xl text-7xl font-medium tracking-tighter text-center bg-clip-text bg-linear-to-b from-neutral-50 to-neutral-500 text-transparent`}>
+                    Unleash the Power of intutive finance
+                </h1>
+                <p className="text-sm text-center max-w-3xl mt-12 text-neutral-200">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quo maiores deleniti accusamus est, placeat ut dolorem, optio magni accusantium eius? Recusandae ullam praesentium accusamus necessitatibus vitae, veritatis delectus minus.
+                </p>
 
             </div>
         </section>
