448989d6f4fdb5e1479f4d71dd1f4753fe617084
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-17 23:51:18 +0530

    Learn placeholder state

--- app/sections/Form.jsx
+++ app/sections/Form.jsx
@@ -23,7 +23,7 @@ const Form = () => {
           id="FName"
           type="text"
           placeholder="Enter your First Name"
-          className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out`}
+          className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out placeholder:text-neutral-500`}
         ></input>
         {/*  */}
         <label
