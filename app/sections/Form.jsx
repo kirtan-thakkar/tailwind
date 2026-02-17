5de30ef3e6ec6bbf0f12f13fb7fb909e99beddd6
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-17 23:30:53 +0530

    Learn active state and hover state

--- app/sections/Form.jsx
+++ app/sections/Form.jsx
@@ -13,13 +13,75 @@ const Form = () => {
         Crazy ass form
       </h1>
       <div className="flex flex-col mt-12 gap-2 w-full max-w-sm mx-auto">
-        <label htmlFor="email" className={`${outfit.className} text-lg tracking-tight`}>Email</label>
+        <label
+          htmlFor="FName"
+          className={`${outfit.className} text-lg tracking-tight`}
+        >
+          First Name
+        </label>
+        <input
+          id="FName"
+          type="text"
+          placeholder="Enter your First Name"
+          className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out`}
+        ></input>
+        {/*  */}
+        <label
+          htmlFor="email"
+          className={`${outfit.className} text-lg tracking-tight`}
+        >
+          Email
+        </label>
         <input
           id="email"
           type="text"
           placeholder="Enter your Email address"
           className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out`}
         ></input>
+        {/*  */}
+        <label
+          htmlFor="Com"
+          className={`${outfit.className} text-lg tracking-tight`}
+        >
+          Company
+        </label>
+        <input
+          id="Com"
+          type="text"
+          placeholder="Enter your Company name"
+          className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out`}
+        ></input>
+        {/*  */}
+        <label
+          htmlFor="LName"
+          className={`${outfit.className} text-lg tracking-tight`}
+        >
+          Last Name
+        </label>
+        <input
+          id="LName"
+          type="text"
+          placeholder="Enter your Last Name"
+          className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out`}
+        ></input>
+        {/*  */}
+        <label
+          htmlFor="Number"
+          className={`${outfit.className} text-lg tracking-tight`}
+        >
+          Number
+        </label>
+        <input
+          id="Number"
+          type="text"
+          placeholder="Enter your Phone Number  "
+          className={`${outfit.className} focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-500 focus:bg-neutral-200 w-full px-4 py-2 bg-white rounded-lg shadow-2xl transition-all duration-300 ease-in-out`}
+        ></input>
+        <button
+          className={`${outfit.className} mt-3 text-white bg-neutral-900 rounded-md p-2  cursor-pointer hover:bg-neutral-700 hover:-translate-y-0.5 transition duration-200 ease-in-out active:scale-95`}
+        >
+          Send That mother Fucking Text
+        </button>
       </div>
     </form>
   );
