d938b7c5b68bdc0c973b35aceb57096e84e3898e
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-19 22:03:52 +0530

    Add lenis

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./sections/Hero";
 import Card from "./sections/Card";
 import Form from "./sections/Form";
+import AceCard from "./motion/AceCard";
 export default function Home() {
   return (
     <>
@@ -11,9 +12,11 @@ export default function Home() {
       <section className="bg-white max-w-4xl mx-auto flex items-start justify-center">
         <Card />
       </section>
-      <section className="mt-24 max-w-4xl h-screen mx-auto ">
+      <section className="mt-24 max-w-4xl mx-auto ">
         <Form />
-
+      </section>
+      <section className="bg-neutral-100 max-4xl mx-auto mt-12 flex justify-center items-center h-screen">
+        <AceCard />
       </section>
     </>
   );
