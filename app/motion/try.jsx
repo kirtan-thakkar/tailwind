57beeabaa4d0f565acb4aba0ed7cad5093e324dc
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-21 22:28:21 +0530

    Add content in try.jsx

--- app/motion/try.jsx
+++ app/motion/try.jsx
@@ -1,8 +1,26 @@
 const TryPage = ()=>{
     return(
-        <div>
-            <h1>Try page</h1>
-        </div>
+        <motion.div
+        initial={{
+            opacity:0.5,
+            filter:"blur(10px)",
+            x:-200
+        }}
+        whileInView={{
+            opacity:1,
+            filter:"blur(0px)",
+            x:0
+        }}
+        transition={{
+            duration:0.3,
+            ease:"easeInOut",
+            delay:0.2
+        }}
+        >
+            <motion.h1>
+                This is a try page for motion
+            </motion.h1>
+        </motion.div>
     )
 }
 export default TryPage;
